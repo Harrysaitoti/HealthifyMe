e36424aa87dcb63675b10939ac679a1b02058604
Harrysaitoti <saitotisunkuli@yahoo.com>
2024-05-24 02:57:18 -0700

    frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Users from './pages/Users';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/users" component={Users} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
